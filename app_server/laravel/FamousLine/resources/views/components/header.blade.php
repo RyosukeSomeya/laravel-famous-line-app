5ab3235c0959231790a472c9985f0f2100de22d1
RyosukeSomeya <rx.78.0618@gmail.com>
2020-02-18 00:37:14 +0900

    ページタイトル微修正

--- app_server/laravel/FamousLine/resources/views/components/header.blade.php
+++ app_server/laravel/FamousLine/resources/views/components/header.blade.php
@@ -1,5 +1,5 @@
 <div class="header_title">
-    <h1><a href="/">Famous Line API console</a></h1>
+    <h1 class="container"><a href="/">Famous Line API console</a></h1>
 </div>
 <div class="container">
     <h2 class="page_title">{{ $page_title }}</h2>
